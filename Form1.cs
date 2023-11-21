772b15f02b74ed9b44e041be9e3578d528db7b0d
huetv <tuonghue2303@gmail.com>
2023-11-21 22:14:29 +0700

    first commit

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phan_quan_ly_nhan_su
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
